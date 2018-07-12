40a2fefe4ebf089c27f1e43da6a4cae905d45bdd
Rahul Prasad <rpiitd3@gmail.com>
2018-07-12 10:48:55 +0530

    Update missing_values.py

--- missing_values.py
+++ missing_values.py
@@ -1,7 +1,7 @@
 '''
 Find data type, percentage of null values and number of unique values for all features
 '''
-def info(df,sort = True):
+def info(df,sort = True,save = "No"):
     size = df.shape[0]
     null = df.isnull().sum()
     nuni = df.nunique()
@@ -9,5 +9,7 @@ def info(df,sort = True):
     missing_pct = null*100/size
     df_info  = pd.concat([dtype, missing_pct.round(2),nuni], axis=1, keys=['type','missing','nunique'])
     if sort == True:
-        df_info.sort_values (['missing'],ascending=False,inplace=True) #null_val.sort_values(ascending=False).round(2)
+        df_info.sort_values (['missing'],ascending=False,inplace=True)
+    if (save == "Yes")
+        df_info.to_csv(os.path.join(folder,'info.csv'))
     return df_info
