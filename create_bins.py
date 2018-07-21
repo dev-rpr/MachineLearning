f5cf6a9b94410011557c941d309ad7602f91bffd
Rahul Prasad <rpiitd3@gmail.com>
2018-07-21 20:02:10 +0530

    create bins for numerical features

--- create_bins.py
+++ create_bins.py
@@ -6,7 +6,6 @@ bins = [uniques[i*int(df[col].nunique()/no_bins)] for i in range(no_bins)]
 bins.append(uniques[len(uniques)-1])
 labels = [label_str[i-1:i] for i in range(1,no_bins+1)]
 df[col + '_binned'] = pd.cut(df[col], bins=bins, labels=labels,right=True,include_lowest=True)
-test[col + '_binned'] = pd.cut(test[col], bins=bins, labels=labels,right=True,include_lowest=True)
 df.drop(col,axis=1,inplace=True)
 
 
@@ -20,3 +19,5 @@ for col in col_bins: #5 unique values in each bin
     bins.append(uniques[len(uniques)-1])
     labels = [label_str[i-1:i] for i in range(1,no_bins+1)]
     df[col + '_binned'] = pd.cut(df[col], bins=bins, labels=labels,right=True,include_lowest=True)
+    
+df.drop(col_bins,axis=1,inplace=True)
